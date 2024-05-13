b2ad84ca6c6f5453591e89975829d589d5428987
viktoria00111 <123251981+viktoria00111@users.noreply.github.com>
2024-05-13 11:54:35 +0300

    dz

---
+++ Dz_3/checkers.py
@@ -0,0 +1,17 @@
+import subprocess
+
+
+def checkout_positive(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, encoding="utf-8")
+    if result.returncode == 0 and text in result.stdout:
+        return True
+    else:
+        return False
+
+
+def checkout_negative(cmd, text):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8")
+    if result.returncode != 0 and (text in result.stderr or text in result.stdout):
+        return True
+    else:
+        return False
